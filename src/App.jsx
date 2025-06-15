208651ac396504d79cd711dccb02b15c1919ef8b
Abdul Majeeth <abdulmajeeth870@gmail.com>
2025-06-15 15:00:25 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navigation from "./Navigation.jsx";
+import Hero from "./Hero.jsx";
+
+
+const App=()=>{
+  return(
+    <div>
+      <Navigation/>
+      <Hero/>
+    </div>
+   
+
+  );
+};
+export default App;
